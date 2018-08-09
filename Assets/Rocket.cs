ab6a54a40d23841ddc93eda71eea564bfef9d651
Joseph Alfredo <jalfredopoly@gmail.com>
2018-08-08 18:29:12 -0700

    RigidBody and Audio Source

---
+++ Assets/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody _rigidbody;
+    AudioSource _audioSource;
+    int rcsPower = 100;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void ProcessInput()
+    {
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            // relative to the local coordinate system
+            _rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * 100);
+            if(!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * rcsPower);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * rcsPower);
+        }
+    }
+}
